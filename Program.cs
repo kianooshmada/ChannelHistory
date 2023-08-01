8b59f01f67886782c6155acf2495179411fc8840
kianooshmada <kianooshnike1365@gmail.com>
2023-08-01 18:37:33 +0200

    change static to private static

--- Program.cs
+++ Program.cs
@@ -2,168 +2,173 @@
 using System.Globalization;
 using ExtensionMethods;
 
-try
+internal class Program
 {
-    string fileName = GetValidFileNameFromUser();
-
-    List<ValuesTagFields> valuesTagFieldsList = ReadValuesTagDataFromFile(fileName);
-    List<VariablesTagFields> variablesTagFieldsList = ReadVariablesTagDataFromFile(fileName);
-    List<OutputFields> outputFieldsList = new List<OutputFields>();
-
-    var groupByValuesTagFieldsList = valuesTagFieldsList.GroupBy(d => d.ChannelIndex);
-
-    foreach (var item in groupByValuesTagFieldsList)
+    private static void Main(string[] args)
     {
-        decimal minValue = item.MinByValue().Value;
-        DateTime minTimeStamp = item.MinByTimeStamp().TimeStamp;
-
-        decimal maxValue = item.MaxByValue().Value;
-        DateTime maxTimeStamp = item.MaxByTimeStamp().TimeStamp;
-
-        byte? variableID = variablesTagFieldsList.Where(v => v.ChannelIndex == item.Key).FirstOrDefault()?.VariableID;
-
-        outputFieldsList.Add(new OutputFields
+        try
         {
-            VariableID = variableID ?? 0,
-            MinValue = minValue,
-            MinTimeStamp = minTimeStamp,
-            MaxValue = maxValue,
-            MaxTimeStamp = maxTimeStamp
-        });
-    }
+            string fileName = GetValidFileNameFromUser();
 
-    WriteInNewTextFile(outputFieldsList);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message, Console.ForegroundColor = ConsoleColor.Red);
-}
+            List<ValuesTagFields> valuesTagFieldsList = ReadValuesTagDataFromFile(fileName);
+            List<VariablesTagFields> variablesTagFieldsList = ReadVariablesTagDataFromFile(fileName);
+            List<OutputFields> outputFieldsList = new List<OutputFields>();
 
-static string GetValidFileNameFromUser()
-{
-    string? fileName;
-    do
-    {
-        Console.WriteLine("Enter the file name:");
-        fileName = Console.ReadLine();
+            var groupByValuesTagFieldsList = valuesTagFieldsList.GroupBy(d => d.ChannelIndex);
 
-        if (string.IsNullOrEmpty(fileName))
-        {
-            Console.WriteLine("File name can't be empty!", Console.ForegroundColor = ConsoleColor.Yellow);
-        }
-
-    } while (string.IsNullOrEmpty(fileName));
-
-    return fileName.Replace(" ", "").ToUpper();
-}
-
-
-static List<ValuesTagFields> ReadValuesTagDataFromFile(string fileName)
-{
-    var valuesTagFieldsList = new List<ValuesTagFields>();
-    var cultureInfo = new CultureInfo("en-US");
-
-    using (StreamReader reader = new StreamReader(fileName))
-    {
-        string? line;
-        bool readingValues = false;
-
-        while ((line = reader.ReadLine()) != null)
-        {
-            if (line.StartsWith("-VALUES-"))
+            foreach (var item in groupByValuesTagFieldsList)
             {
-                readingValues = true;
-                continue;
-            }
+                decimal minValue = item.MinByValue().Value;
+                DateTime minTimeStamp = item.MinByTimeStamp().TimeStamp;
 
-            if (readingValues)
-            {
-                string[] splittedItems = line.Split(';');
+                decimal maxValue = item.MaxByValue().Value;
+                DateTime maxTimeStamp = item.MaxByTimeStamp().TimeStamp;
 
-                ValuesTagFields valuesTagFields = new ValuesTagFields
+                byte? variableID = variablesTagFieldsList.Where(v => v.ChannelIndex == item.Key).FirstOrDefault()?.VariableID;
+
+                outputFieldsList.Add(new OutputFields
                 {
-                    ChannelIndex = splittedItems[0].Split(':')[0],
-                    Value = Convert.ToDecimal(splittedItems[0].Split(':')[1]),
-                    TimeStamp = DateTime.ParseExact(splittedItems[2], "dd.MM.yyyy HH:mm:ss.fff", cultureInfo)
-                };
-
-                valuesTagFieldsList.Add(valuesTagFields);
+                    VariableID = variableID ?? 0,
+                    MinValue = minValue,
+                    MinTimeStamp = minTimeStamp,
+                    MaxValue = maxValue,
+                    MaxTimeStamp = maxTimeStamp
+                });
             }
+
+            WriteInNewTextFile(outputFieldsList);
         }
-    }
-
-    return valuesTagFieldsList;
-}
-
-static List<VariablesTagFields> ReadVariablesTagDataFromFile(string fileName)
-{
-    var variablesTagFieldsList = new List<VariablesTagFields>();
-
-    using (StreamReader reader = new StreamReader(fileName))
-    {
-        string? line;
-        bool readingValues = false;
-
-        while ((line = reader.ReadLine()) != null)
+        catch (Exception ex)
         {
-            if (line.StartsWith("-VARIABLES-"))
-            {
-                readingValues = true;
-                continue;
-            }
-            if (line.StartsWith("-VALUES-"))
-            {
-                readingValues = false;
-                break;
-            }
-
-            if (readingValues)
-            {
-                string[] splittedItems = line.Split("=");
-
-                VariablesTagFields variablesTagFields = new VariablesTagFields
-                {
-                    ChannelIndex = splittedItems[0],
-                    VariableID = Convert.ToByte(splittedItems[1])
-                };
-                variablesTagFieldsList.Add(variablesTagFields);
-            }
+            Console.WriteLine(ex.Message, Console.ForegroundColor = ConsoleColor.Red);
         }
     }
-
-    return variablesTagFieldsList;
-}
-
-static void WriteInNewTextFile(List<OutputFields> outputFieldsList)
-{
-    ConsoleKey response;
-    do
+    private static string GetValidFileNameFromUser()
     {
-        Console.Write("Do you want to save the new file in Desktop? [y/n] ");
-        response = Console.ReadKey(false).Key;
-        if (response != ConsoleKey.Enter)
-            Console.WriteLine();
-
-    } while (response != ConsoleKey.Y && response != ConsoleKey.N);
-
-    string filePath = string.Empty;
-
-    if (response == ConsoleKey.N)
-    {
+        string? fileName;
         do
         {
-            Console.WriteLine("Enter the path for saving the file:");
-            filePath = Console.ReadLine()!;
-        } while (string.IsNullOrEmpty(filePath));
+            Console.WriteLine("Enter the file name:");
+            fileName = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                Console.WriteLine("File name can't be empty!", Console.ForegroundColor = ConsoleColor.Yellow);
+            }
+
+        } while (string.IsNullOrEmpty(fileName));
+
+        return fileName.Replace(" ", "").ToUpper();
     }
-    else
-        filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-    using (StreamWriter outputFile = new StreamWriter(Path.Combine(filePath!, "OutputFile.txt")))
+
+    private static List<ValuesTagFields> ReadValuesTagDataFromFile(string fileName)
     {
-        foreach (var outputFields in outputFieldsList)
-            outputFile.WriteLine($" Variable ID: {outputFields.VariableID}, Min Value: {outputFields.MinValue}, Min TimeStamp: {outputFields.MinTimeStamp}, Max Value: {outputFields.MaxValue}, Max TimeStamp: {outputFields.MaxTimeStamp}");
+        var valuesTagFieldsList = new List<ValuesTagFields>();
+        var cultureInfo = new CultureInfo("en-US");
 
-        Console.WriteLine("The output file created successfully in the path {0}.", filePath, Console.ForegroundColor = ConsoleColor.Green);
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string? line;
+            bool readingValues = false;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith("-VALUES-"))
+                {
+                    readingValues = true;
+                    continue;
+                }
+
+                if (readingValues)
+                {
+                    string[] splittedItems = line.Split(';');
+
+                    ValuesTagFields valuesTagFields = new ValuesTagFields
+                    {
+                        ChannelIndex = splittedItems[0].Split(':')[0],
+                        Value = Convert.ToDecimal(splittedItems[0].Split(':')[1]),
+                        TimeStamp = DateTime.ParseExact(splittedItems[2], "dd.MM.yyyy HH:mm:ss.fff", cultureInfo)
+                    };
+
+                    valuesTagFieldsList.Add(valuesTagFields);
+                }
+            }
+        }
+
+        return valuesTagFieldsList;
+    }
+
+    private static List<VariablesTagFields> ReadVariablesTagDataFromFile(string fileName)
+    {
+        var variablesTagFieldsList = new List<VariablesTagFields>();
+
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string? line;
+            bool readingValues = false;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith("-VARIABLES-"))
+                {
+                    readingValues = true;
+                    continue;
+                }
+                if (line.StartsWith("-VALUES-"))
+                {
+                    readingValues = false;
+                    break;
+                }
+
+                if (readingValues)
+                {
+                    string[] splittedItems = line.Split("=");
+
+                    VariablesTagFields variablesTagFields = new VariablesTagFields
+                    {
+                        ChannelIndex = splittedItems[0],
+                        VariableID = Convert.ToByte(splittedItems[1])
+                    };
+                    variablesTagFieldsList.Add(variablesTagFields);
+                }
+            }
+        }
+
+        return variablesTagFieldsList;
+    }
+
+    private static void WriteInNewTextFile(List<OutputFields> outputFieldsList)
+    {
+        ConsoleKey response;
+        do
+        {
+            Console.Write("Do you want to save the new file in Desktop? [y/n] ");
+            response = Console.ReadKey(false).Key;
+            if (response != ConsoleKey.Enter)
+                Console.WriteLine();
+
+        } while (response != ConsoleKey.Y && response != ConsoleKey.N);
+
+        string filePath = string.Empty;
+
+        if (response == ConsoleKey.N)
+        {
+            do
+            {
+                Console.WriteLine("Enter the path for saving the file:");
+                filePath = Console.ReadLine()!;
+            } while (string.IsNullOrEmpty(filePath));
+        }
+        else
+            filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(filePath!, "OutputFile.txt")))
+        {
+            foreach (var outputFields in outputFieldsList)
+                outputFile.WriteLine($" Variable ID: {outputFields.VariableID}, Min Value: {outputFields.MinValue}, Min TimeStamp: {outputFields.MinTimeStamp}, Max Value: {outputFields.MaxValue}, Max TimeStamp: {outputFields.MaxTimeStamp}");
+
+            Console.WriteLine("The output file created successfully in the path {0}.", filePath, Console.ForegroundColor = ConsoleColor.Green);
+        }
     }
 }
